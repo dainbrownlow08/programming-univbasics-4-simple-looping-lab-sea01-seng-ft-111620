d5bb1b026510feb09b0ce00887c0e89f13e82dc8
Dain Brownlow <dain.brownlow@outlook.com>
2020-10-29 23:45:20 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -17,10 +17,9 @@ def output_array(array)
 end
 
 def return_string_array(array)
-  count = 0
-  while count < array.length do
-    array[count].to_s
-    count += 1
+  end_array = []
+  for i in array do
+  end_array << array[i].to_s
   end
-  return array
+  return end_array
 end
